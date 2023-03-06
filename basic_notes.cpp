d3da8eaa03af7c6b0fc9fe92e33f7a64292cf281
Jesse Q Morrow <jqtomorrow@Gmail.com>
2023-03-05 17:01:06 -0800

    commits basic notes 1-4

---
+++ basic_notes.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int notmain();
+
+int main(){
+
+    cout << "Keep calm";
+    cout << "and";
+    cout << "carry on";
+
+    notmain();
+    
+    return 0;
+}
+
+
+int notmain(){
+    cout << "Keep Calm";
+    cout << endl;
+    cout << "and";
+    cout << endl;
+    cout << "carry on";
+
+    return 0;
+}
---
+++ basic_notes3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int drivingYear;
+    int drivingAge;
+    int numStates;
+
+    drivingYear = 2014;
+    drivingAge = 18;
+    numStates = 10;
+
+//this is a comment, i think
+
+    cout << "In ";
+    cout << drivingYear;
+    cout << ", the driving age is ";
+    cout << drivingAge;
+    cout << ".";
+    cout << endl;
+    cout << numStates;
+    cout << " states have exceptions.";
+
+    return 0;
+}
