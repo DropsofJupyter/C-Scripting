6d67278a2a6cecadffade442e40c6fba6193d1f2
Jesse Q Morrow <jqtomorrow@Gmail.com>
2023-03-07 08:20:06 -0800

    commits new test file

---
+++ Programs/test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long Long;
+    int Int;
+
+    cout << Long << endl;
+    cout << Int << endl;
+
+    return 0;
+}
